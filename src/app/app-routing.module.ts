8b3e8d15d7911e812435208891f3670e01b8facd
siautong <siautong2003@gmail.com>
2023-11-27 19:30:21 +0700

    Update Angular + Tambah data mahasiswa

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { AdminComponent } from './admin/admin.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { Dashboard2Component } from './dashboard2/dashboard2.component';
+import { Dashboard3Component } from './dashboard3/dashboard3.component';
+import { MahasiswaComponent } from './mahasiswa/mahasiswa.component';
+const routes: Routes = [
+{ path: "admin", component: AdminComponent },
+{ path: "login", component: LoginComponent },
+{ path: "register", component: RegisterComponent },
+{ path: "dashboard", component: DashboardComponent },
+{ path: "dashboard2", component: Dashboard2Component },
+{ path: "dashboard3", component: Dashboard3Component },
+{ path: "mahasiswa", component: MahasiswaComponent },
+{ path: "", redirectTo: "login", pathMatch: "full" }
+]
+@NgModule({
+imports: [RouterModule.forRoot (routes)],
+exports: [RouterModule]
+})
+export class AppRoutingModule { }
